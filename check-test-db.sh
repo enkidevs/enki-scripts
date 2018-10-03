73b53ae0190326439ad5463e3d29e4e3da32bf65
Mihai Bercu <mihaiberq@gmail.com>
2018-10-03 10:37:26 +0300

    remove do

--- check-test-db.sh
+++ check-test-db.sh
@@ -7,7 +7,6 @@ nonzeroCollections=""
 collectionsCount=0
 
 for col in $collections
-do
     prefix="${col:0:6}"
     collectionsCount=$((collectionsCount + 1))
     if [ $prefix != "system" ]; then
@@ -30,4 +29,4 @@ echo -e "Test fails for: \n${nonzeroCollections}"
 exit 1
 
 
-# docker exec -it --env MONGODB_URL="${MONGODB_URL}" "${MONGO_CONTAINER}" curl https://raw.githubusercontent.com/enkidevs/enki-scripts/master/check-test-db.sh | MONGODB_URL="$MONGODB_URL" sh
+# docker exec -it --env MONGODB_URL="${MONGODB_URL}" "${MONGO_CONTAINER}" curl https://raw.githubusercontent.com/enkidevs/enki-scripts/test-script/check-test-db.sh | MONGODB_URL="$MONGODB_URL" sh
